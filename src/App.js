9acd22f11bb455c9f24a1e3b5190bf55a83dde33
Slavich <ss_slavich@mail.ru>
2022-04-29 20:30:10 +0300

    Done block calc panel to pointer-events

--- src/App.js
+++ src/App.js
@@ -14,56 +14,66 @@ import { reducer } from "./state/reducer";
 import { useDispatch, useSelector } from "react-redux";
 import { constructorSVG, runtimeSVG } from "./image";
 import { changeAppMode, CONSTRUCTOR, RUNTIME } from "./redux/app_reducer";
-import s from "./App.module.css"
+import s from "./App.module.css";
 
-const getAppMode = state => state.app.appMode
+const getAppMode = (state) => state.app.appMode;
 
 function App(props) {
-  const dispatch = useDispatch()
-  const appMode = useSelector(getAppMode)
- // const [mode, setMode] = useState(appMode)
-//  useEffect( () => console.log('appMode',appMode),
-//  [appMode])
-  
-  const styleActive = {
-      background: "#FFFFFF",
-      border: "1px solid #E2E3E5" 
-    }
+  const dispatch = useDispatch();
+  const appMode = useSelector(getAppMode);
+  // const [mode, setMode] = useState(appMode)
+  //  useEffect( () => console.log('appMode',appMode),
+  //  [appMode])
 
-    const styleDeactive = {
-      // background: "#FFFFFF",
-      // border: "1px solid #E2E3E5" 
-    }
+  // const style = {
+  //   pointerEvents: "none"
+
+  // }
 
   const onBtnClick = (e) => {
-
     if (e.target.className == `${s.btn}` && appMode == CONSTRUCTOR) {
-      
-        dispatch(changeAppMode(RUNTIME))
-    } else dispatch(changeAppMode(CONSTRUCTOR))
+      dispatch(changeAppMode(RUNTIME));
+    } else dispatch(changeAppMode(CONSTRUCTOR));
 
-    
-    
-  //  if (appMode == RUNTIME) {
-  //    e.target.className = e.target.className + ' ' + s.active
-  //  }
-  }
+    //  if (appMode == RUNTIME) {
+    //    e.target.className = e.target.className + ' ' + s.active
+    //  }
+  };
 
-  const constructorClassName = (appMode == CONSTRUCTOR) ? `${s.btn} ${s.active}` : s.btn
-  const runtimeClassName = (appMode == RUNTIME) ? `${s.btn} ${s.active}` : s.btn
+  const constructorBtnClassName =
+    appMode == CONSTRUCTOR ? `${s.btn} ${s.active}` : s.btn;
+  const runtimeBtnClassName =
+    appMode == RUNTIME ? `${s.btn} ${s.active}` : s.btn;
+
+  const constructorClassName = (appMode == RUNTIME) ? s.calculator : s.deactive
+  const runtimeClassName = appMode == CONSTRUCTOR ? s.constructor :  s.deactive
+
+  // console.log('constructorClassName ', constructorClassName)
+  // console.log('runtimeClassName ', runtimeClassName)
 
   return (
     <DndProvider backend={HTML5Backend}>
       <div className={s.App}>
         <div className={s.header}>
           <div className={s.btnContainer}>
-            <div className={runtimeClassName}  onClick={ (e) => onBtnClick(e) }>{runtimeSVG} Runtime</div>
-            <div className={constructorClassName}  onClick={ (e) => onBtnClick(e)}>{constructorSVG} Constructor</div>
+            <div className={runtimeBtnClassName} onClick={(e) => onBtnClick(e)}>
+              {runtimeSVG} Runtime
+            </div>
+            <div
+              className={constructorBtnClassName}
+              onClick={(e) => onBtnClick(e)}
+            >
+              {constructorSVG} Constructor
+            </div>
           </div>
         </div>
         <div className={s.content}>
-          <Constructor disabled={true} className={s.constructor} />
-          <Calculator className={s.calculator} />
+          <div className={constructorClassName}>
+            <Constructor />
+          </div>
+          <div className={runtimeClassName}>
+            <Calculator />
+          </div>
         </div>
       </div>
     </DndProvider>
